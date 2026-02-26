586124f5cffd23e47c1dc202ccae841d8655456e
Artabarean <atabarea@c2r1s3.42madrid.com>
2026-02-26 14:27:11 +0100

    .hpp start

--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -1,16 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   zombie.hpp                                         :+:      :+:    :+:   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: atabarea <atabarea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/26 14:16:56 by atabarea          #+#    #+#             */
-/*   Updated: 2026/02/26 14:17:33 by atabarea         ###   ########.fr       */
+/*   Updated: 2026/02/26 14:26:41 by atabarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ZOMBIE_HPP
 # define ZOMBIE_HPP
+
+# include <cstring>
+# include <cstdio>
+# include <cstdlib>
+# include <iostream>
+
+class Zombie{
+
+	Zombie();
+	~Zombie();
+	public:
+		
+	private:
+		std::string name;
+		
+};
 
 #endif
