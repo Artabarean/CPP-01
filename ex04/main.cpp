b51b8da375544a2428602deec844fa7af13e50c8
Artabarean <atabarea@c2r3s5.42madrid.com>
2026-03-05 12:06:11 +0100

    ex04 done

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: atabarea <atabarea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/04 11:38:14 by atabarea          #+#    #+#             */
-/*   Updated: 2026/03/04 12:57:48 by atabarea         ###   ########.fr       */
+/*   Updated: 2026/03/05 11:13:34 by atabarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,49 @@
 #include <string>
 #include <cstdlib>
 #include <fstream>
+
+int	Sed(std::string filename, std::string s1, std::string s2)
+{
+	std::string line;
+	std::size_t found;
+	std::size_t iterator;
+	
+	std::ifstream inputFile(filename.c_str());
+	if (!inputFile.is_open())
+	{
+		std::cout << "Error: Could not open the file." << std::endl;
+		return (1);
+	}
+	filename.append(".replace");
+	std::ofstream outputFile(filename.c_str());
+	if (!outputFile.is_open())
+	{
+		std::cout << "Error: Could not open or create the replacement file." << std::endl;
+		return (1);
+	}
+	while (std::getline(inputFile, line))
+	{
+		found = line.find(s1);
+		if (found != std::string::npos)
+		{
+			while (found != std::string::npos)
+			{
+				line.erase(found, s1.length());
+				iterator = found;
+				for (size_t i = 0; i < s2.length(); i++)
+				{
+					line.insert(iterator, 1, s2[i]);
+					iterator++;
+				}
+				found = line.find(s1);
+			}
+		}
+		outputFile << line << std::endl;
+	}
+	inputFile.close();
+	outputFile.close();
+	return (0);
+}
 
 int main(int argc, char *argv[])
 {
@@ -25,17 +68,7 @@ int main(int argc, char *argv[])
 	std::string filename = argv[1];
 	std::string s1 = argv[2];
 	std::string s2 = argv[3];
-	std::ifstream inputFile(filename.c_str());
-	if (!inputFile.is_open())
-	{
-		std::cout << "Error: Could not open the file." << std::endl;
+	if (Sed(filename, s1, s2) == 1)
 		return (1);
-	}
-	std::string line;
-	while (std::getline(inputFile, line))
-	{
-		
-	}
-	inputFile.close();
 	return (0);
 }
---
+++ ex05/Harl.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atabarea <atabarea@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/05 11:15:06 by atabarea          #+#    #+#             */
+/*   Updated: 2026/03/05 11:49:27 by atabarea         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+# include <iostream>
+# include <string>
+
+class	Harl
+{
+	public:
+		Harl(void);
+		~Harl(void);
+		void complain( std::string level );
+	private:
+		void	debug( void );
+		void	info( void );
+		void	warning( void );
+		void	error( void );
+};
+
+#endif
