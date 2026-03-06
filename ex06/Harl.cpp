5ff9fe51633a2511eda2e38075ebca19b9a42086
Artabarean <atabarea@c2r3s5.42madrid.com>
2026-03-06 11:44:11 +0100

    project finished

--- ex06/Harl.cpp
+++ ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: atabarea <atabarea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/05 11:15:04 by atabarea          #+#    #+#             */
-/*   Updated: 2026/03/06 10:13:23 by atabarea         ###   ########.fr       */
+/*   Updated: 2026/03/06 11:42:43 by atabarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,38 +22,70 @@ Harl::~Harl(void)
 
 void	Harl::debug( void )
 {
+	std::cout << "[ DEBUG ]" << std::endl;
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple"
-		<< "-pickle-specialketchup burger. I really do!" << std::endl;
+		<< "-pickle-specialketchup burger. I really do!\n" << std::endl;
 }
 
 void	Harl::info(void)
 {
+	std::cout << "[ INFO ]" << std::endl;
 	std::cout << "I cannot believe adding extra bacon costs more money. "
 		<< "You didn't putenough bacon in my burger! If you did, "
-		<< "I wouldn't be asking for more!" << std::endl;
+		<< "I wouldn't be asking for more!\n" << std::endl;
 }
 
 void	Harl::warning(void)
 {
+	std::cout << "[ WARNING ]" << std::endl;
 	std::cout << "I think I deserve to have some extra bacon for free. "
 		<< "I've been coming for years, whereas you started working here"
-		<< " just last month." << std::endl;
+		<< " just last month.\n" << std::endl;
 }
 
 void	Harl::error(void)
 {
-	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+	std::cout << "[ ERROR ]" << std::endl;
+	std::cout << "This is unacceptable! I want to speak to the manager now.\n" << std::endl;
+}
+
+void	Harl::generic(void)
+{
+	std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
 }
 
 void	Harl::complain(std::string level)
 {
-	const std::string issues[4] = {"DEBUG","INFO","WARNING","ERROR"};
-	void	(Harl::*levels[4])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
-	for (size_t i = 0; i < 4; i++)
+	const std::string	issues[4] = {"DEBUG","INFO","WARNING","ERROR"};
+	t_levels			type[5] = {DEBUG, INFO, WARNING, ERROR, DEFAULT};
+	int					complaint;
+	int					i = 0;
+
+	while (i < 5)
 	{
-		if (level == issues[i])
+		if (issues[i] == level)
 		{
-			(this->*levels[i])();
+			complaint = type[i];
+			break;
 		}
+		else
+			complaint = type[i];
+		i++;
+	}
+	switch (complaint)
+	{
+		case 0:
+			this->debug();
+		case 1:
+			this->info();
+		case 2:
+			this->warning();
+		case 3:
+		{
+			this->error();
+			break;
+		}
+		case 4:
+			this->generic();
 	}
 } 
---
+++ ex06/Harl.hpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atabarea <atabarea@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/05 11:15:06 by atabarea          #+#    #+#             */
+/*   Updated: 2026/03/06 11:19:06 by atabarea         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+# include <iostream>
+# include <string>
+
+typedef enum
+{
+	DEBUG,
+	INFO,
+	WARNING,
+	ERROR,
+	DEFAULT
+}	t_levels;
+
+class	Harl
+{
+	public:
+		Harl(void);
+		~Harl(void);
+		void complain( std::string level );
+	private:
+		void	debug( void );
+		void	info( void );
+		void	warning( void );
+		void	error( void );
+		void	generic(void);
+};
+
+#endif
